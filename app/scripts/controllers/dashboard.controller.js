b12f3017e142f9f98812d9ecc0db81671004e85a
luule_hackathon <luu.le@dnata.com>
2015-09-04 22:57:19 +0400

    update create idea.

--- app/scripts/controllers/dashboard.controller.js
+++ app/scripts/controllers/dashboard.controller.js
@@ -1,13 +1,41 @@
-'use strict';
-
 /**
- * @ngdoc function
- * @name inceptionApp.controller:DashboardController
- * @description
- * # DashboardController
- * Controller of the inceptionApp
+ * Created by s727223 on 04/09/2015.
  */
- angular.module('inceptionApp')
- .controller('DashboardController', function(){
+ (function(){
+  'use strict';
 
-});
+  angular
+    .module('inceptionApp')
+    .controller('DashboardController', DashboardController);
+
+  DashboardController.$inject = ['$scope', '$mdDialog'];
+
+  function DashboardController($scope, $mdDialog){
+    var vm = this;
+    vm.createIdea = createIdea;
+
+    //==================== Function declaration ====================
+
+
+  function createIdea(event){
+      $mdDialog.show({
+        controller: "CreateIdeaController",
+        controllerAs: "vm",
+        templateUrl: 'views/create-idea.html',
+        parent: angular.element(document.body),
+        targetEvent: event,
+        clickOutsideToClose:false
+      })
+        .then(function(answer) {
+          // getTripsData().then(function(){
+          //   initTripsProperty();
+          // });
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+    };
+   
+  }
+
+})();
+
