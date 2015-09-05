00caa32595613bf27bac067333ba0ffc823f61cb
Diep The Anh <theanh.mx@gmail.com>
2015-09-05 08:38:00 +0400

    add data_ideas.js

--- app/assets/data_ideas.js
+++ app/assets/data_ideas.js
@@ -4,6 +4,7 @@ var data_ideas = [
         "thumpUp": 10,
         "title": "Sell Vietnamese food in Dubai",
         "ownerName": "Dat Nguyen",
+        "ownerID" : 1,
         "description": "Idea description",
         "thumpDown": 2,
         "possibility": 90,
@@ -100,6 +101,7 @@ var data_ideas = [
         "id": 2,
         "title": "QR code to buy product from supermarket, not require to wait in line",
         "ownerName": "Hien Pham",
+        "ownerID" : 3,
         "description": "Idea description",
         "thumpUp": 50,
         "thumpDown": 10,
@@ -197,6 +199,7 @@ var data_ideas = [
         "id": 3,
         "title": "Driver identify",
         "ownerName": "Hung Thai",
+        "ownerID" : 4,
         "description": "The car is only power on for the owner of the car",
         "thumpUp": 100,
         "thumpDown": 20,
@@ -323,7 +326,204 @@ var data_ideas = [
                 ]
             }
         ]
+    },
+    {
+        "id": 4,
+        "thumpUp": 13,
+        "title": "DuStop a stop over application",
+        "ownerName": "Anh Diep",
+        "ownerID" : 5,
+        "description": "Idea description",
+        "thumpDown": 1,
+        "possibility": 100,
+        "createDate": "2015-09-05 07:12:00",
+        "modifyDate": "2015-09-05 08:00:00",
+        "labels": ["Dubai", "stopover", "travel", "grouping", "deal", "skyward"],
+        "privacy": "onlyme",
+        "categories": [
+            {
+                "id": 1,
+                "name": "Introduction",
+                "type": 1,
+                "sections": [
+                    {
+                        "id": 1,
+                        "thumpUp": 18,
+                        "thumpDown": 5,
+                        "privacy": "public",
+                        "history": [
+                            {
+                                "description": "encourage people to stopover at Dubai",
+                                "createDate": "2015-09-05 08:12:30"
+                            },
+                            {
+                                "description": "The first step to expand over the world",
+                                "createDate": "2015-09-05 13:12:30"
+                            }
+                        ],
+                        "comments": [
+                            {
+                                "id": 1,
+                                "value": "that's a good idea",
+                                "owner": {
+                                    "name": "Hung Thai",
+                                    "avatarURL": "../images/avatar/hung_thai.jpg"
+                                }
+                            },
+                            {
+                                "id": 2,
+                                "value": "Hey! lets make it free",
+                                "owner": {
+                                    "name": "Luu Le",
+                                    "avatarURL": "../images/avatar/hien_pham.jpg"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "name": "Blocker",
+                "type": 1,
+                "sections": [
+                    {
+                        "id": 1,
+                        "thumpUp": 19,
+                        "thumpDown": 12,
+                        "privacy": "public",
+                        "history": [
+                            {
+                                "description": "Advertise Dubai to the world",
+                                "createDate": "2015-09-05 13:12:30"
+                            },
+                            {
+                                "description": "The first step to expand over the world",
+                                "createDate": "2015-09-05 13:12:30"
+                            }
+                        ],
+                        "comments": [
+                            {
+                                "id": 1,
+                                "value": "Section comment 1",
+                                "owner": {
+                                    "name": "Hung Thai",
+                                    "avatarURL": "../images/avatar/hung_thai.jpg"
+                                }
+                            },
+                            {
+                                "id": 2,
+                                "value": "Section comment 2",
+                                "owner": {
+                                    "name": "Hien Pham",
+                                    "avatarURL": "../images/avatar/hien_pham.jpg"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "thumpUp": 530,
+        "title": "Make laptops with liquid cooler",
+        "ownerName": "Anh Diep",
+        "ownerID" : 5,
+        "description": "liquid cooler will be implement on laptops",
+        "thumpDown": 1,
+        "possibility": 100,
+        "createDate": "2015-09-05 07:12:00",
+        "modifyDate": "2015-09-05 08:00:00",
+        "labels": ["laptop", "liquid cooler", "pc", "gaming"],
+        "privacy": "onlyme",
+        "categories": [
+            {
+                "id": 1,
+                "name": "Introduction",
+                "type": 1,
+                "sections": [
+                    {
+                        "id": 1,
+                        "thumpUp": 130,
+                        "thumpDown": 3,
+                        "privacy": "public",
+                        "history": [
+                            {
+                                "description": "how to make laptops with liquid cooler",
+                                "createDate": "2015-09-05 08:12:30"
+                            },
+                            {
+                                "description": "it would be very cool",
+                                "createDate": "2015-09-05 13:12:30"
+                            }
+                        ],
+                        "comments": [
+                            {
+                                "id": 1,
+                                "value": "that's a good idea",
+                                "owner": {
+                                    "name": "Hung Thai",
+                                    "avatarURL": "../images/avatar/hung_thai.jpg"
+                                }
+                            },
+                            {
+                                "id": 2,
+                                "value": "Hey! lets make it free",
+                                "owner": {
+                                    "name": "Luu Le",
+                                    "avatarURL": "../images/avatar/hien_pham.jpg"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "name": "Blocker",
+                "type": 1,
+                "sections": [
+                    {
+                        "id": 1,
+                        "thumpUp": 19,
+                        "thumpDown": 12,
+                        "privacy": "public",
+                        "history": [
+                            {
+                                "description": "Advertise liquid cooler",
+                                "createDate": "2015-09-05 13:12:30"
+                            },
+                            {
+                                "description": "The second step is to make it on mobile phone",
+                                "createDate": "2015-09-05 13:12:30"
+                            }
+                        ],
+                        "comments": [
+                            {
+                                "id": 1,
+                                "value": "Section comment 1",
+                                "owner": {
+                                    "name": "Hung Thai",
+                                    "avatarURL": "../images/avatar/hung_thai.jpg"
+                                }
+                            },
+                            {
+                                "id": 2,
+                                "value": "Section comment 2",
+                                "owner": {
+                                    "name": "Hien Pham",
+                                    "avatarURL": "../images/avatar/hien_pham.jpg"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
     }
+
 ];
 
 
